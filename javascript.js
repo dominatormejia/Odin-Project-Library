9d2c086cb25517690c1f99786ab5c94113dd1396
Dominic Mejia <206021858+dominatormejia@users.noreply.github.com>
2025-06-08 00:02:44 -0500

    Code Organization with Modules and Class

--- javascript.js
+++ javascript.js
@@ -1,30 +1,30 @@
-const main = document.querySelector("main");
-const buttonDisplay = document.querySelector(".display");
-const form = document.querySelector("form");
-const submit = document.querySelector(".submit");
-const select = document.querySelector("#read");
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor");
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
   }
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.id = crypto.randomUUID();
+
+  updateRead(value) {
+    this.read = value;
+  }
 }
 
-Book.prototype.updateRead = function (value) {
-  this.read = value;
-};
-
-function addBookToLibrary(title, author, pages, read) {
+const addBookToLibrary = function (title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-}
-function displayBooks() {
+};
+
+const displayBooks = function () {
+  const main = document.querySelector("main");
+  main.querySelectorAll(".card").forEach((card) => {
+    card.remove();
+  });
+
   for (let i = 0; i < myLibrary.length; i++) {
     const newDiv = document.createElement("div");
     const newTitle = document.createElement("h2");
@@ -69,54 +69,54 @@ function displayBooks() {
       myLibrary[i].updateRead(e.target.value);
     });
   }
-}
+};
 
-buttonDisplay.addEventListener("click", () => {
-  if (form.classList.contains("hidden")) {
-    form.classList.remove("hidden");
-  } else {
-    form.classList.add("hidden");
-  }
-});
+const buttonHandlers = (function () {
+  const form = document.querySelector("form");
+  const buttonDisplay = document.querySelector(".display");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const titleInput = document.querySelector("#title");
-  const authorInput = document.querySelector("#author");
-  const pagesInput = document.querySelector("#page");
-  const readInput = document.querySelector("#read");
-
-  addBookToLibrary(
-    titleInput.value,
-    authorInput.value,
-    pagesInput.value,
-    readInput.value
-  );
-
-  main.querySelectorAll(".card").forEach((card) => {
-    card.remove();
+  buttonDisplay.addEventListener("click", () => {
+    if (form.classList.contains("hidden")) {
+      form.classList.remove("hidden");
+    } else {
+      form.classList.add("hidden");
+    }
   });
 
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const titleInput = document.querySelector("#title");
+    const authorInput = document.querySelector("#author");
+    const pagesInput = document.querySelector("#page");
+    const readInput = document.querySelector("#read");
+
+    addBookToLibrary(
+      titleInput.value,
+      authorInput.value,
+      pagesInput.value,
+      readInput.value
+    );
+
+    displayBooks();
+
+    form.reset();
+    form.classList.add("hidden");
+  });
+})();
+
+const templateBooks = (function () {
+  addBookToLibrary(
+    "The Hobbit",
+    "by J.R.R. Tolkien,",
+    "295 pages",
+    "not read yet"
+  );
+  addBookToLibrary(
+    "The Way of Kings",
+    "by Brandon Sanderson",
+    "1,007 pages",
+    "have read"
+  );
   displayBooks();
-
-  form.reset();
-  form.classList.add("hidden");
-});
-
-addBookToLibrary(
-  "The Hobbit",
-  "by J.R.R. Tolkien,",
-  "295 pages",
-  "not read yet"
-);
-addBookToLibrary(
-  "The Way of Kings",
-  "by Brandon Sanderson",
-  "1,007 pages",
-  "have read"
-);
-
-displayBooks(myLibrary);
-
-console.log(myLibrary);
+})();
